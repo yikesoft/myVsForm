b621bd3c4d5a9dd46b3cc056cd594716a341b307
myName <myMail@abc.com>
2011-10-28 17:28:18 +0800

    增加一个vs解决方案

---
+++ myForm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace myForm {
+    class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine( "这是中文输入" );
+            Console.ReadLine();
+        }
+    }
+}
